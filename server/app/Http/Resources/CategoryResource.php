4548c575e80378afc2b53df3a223007673ac0508
khamza <khamza.khudayberdiyev@gmail.com>
2023-06-14 11:48:51 +0500

    refactoring

--- server/app/Http/Resources/CategoryResource.php
+++ server/app/Http/Resources/CategoryResource.php
@@ -10,8 +10,9 @@ class CategoryResource extends JsonResource
   public function toArray($request)
   {
     return [
-      'title' => $this->title,
-      'slug'  => $this->slug,
+      'title'    => $this->title,
+      'slug'     => $this->slug,
+      'products' => $this->products,
     ];
   }
 }
